419262544ec2dc1514263e338887cf8d4bdcf074
Vishakh Sandwar <vishakhs74@gmail.com>
2023-06-29 16:32:06 -0400

    first attempt ui companies card

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -12,13 +12,13 @@ const Company = ({ directionLeft, source, status, name }) => {
             transition={{ duration: 1 }}
             whileInView={{ opacity: 1, x: 0 }}
             src={source}
-            className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out bg-white'
+            className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out bg-white'
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100 flex flex-col items-center'>
+                <p className='text-base md:text-3xl font-bold text-black opacity-100 flex flex-col items-center'>
                     {status ? "Alumni" : "Partner"}
-                    <span className='text-xl font-normal text-gray-500' >{name}</span>
+                    <span className='text-sm md:text-xl font-normal text-gray-500' >{name}</span>
                 </p>
             </div>
         </div>
