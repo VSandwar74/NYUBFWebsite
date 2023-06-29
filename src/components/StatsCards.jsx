b5c91ea4c1b69bf87c9652773026c6423e26fb6b
Vishakh Sandwar <vishakhs74@gmail.com>
2023-06-29 16:21:47 -0400

    fixed ui for hero, welcome and stats

--- src/components/StatsCards.jsx
+++ src/components/StatsCards.jsx
@@ -9,13 +9,13 @@ const StatsCard = () => {
         transition={{ duration: 1.5 }}
         className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         
-            <div className="flex flex-col items-center justify-around h-screen">
-                <h3 className='uppercase tracking-[20px] text-white text-2xl'>Statistics</h3>
-                    <div className="flex flex-col py-10 px-20 rounded-l-lg w-[90%] h-fit text-center mt-4">
-                        <p className="text-base">
+            <div className="flex flex-col items-center justify-center md:justify-around h-screen">
+                <h3 className='uppercase tracking-[5px] md:tracking-[20px] text-white text-2xl'>Statistics</h3>
+                    <div className="flex flex-col py-10 px-2 md:px-20 rounded-l-lg w-[90%] h-fit text-center mt-4">
+                        <p className="text-xs md:text-base">
                             NYU BF is a unique student organization devoted to expanding students' understanding of blockchain technology and the FinTech ecosystem, as well as the legal, economic, social, and political challenges posed by these innovative technologies in the US and globally.
                         </p>
-                        <ul>
+                        <ul className="text-xs md:text-base">
                             <li>Our Community
                                 <ul>
                                     <li>817+ Instagram Followers</li>
